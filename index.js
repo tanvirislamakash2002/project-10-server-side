9b933e8b80cee8b73dcbc0a4bc998ecf92cf47e5
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-05-21 01:55:31 +0600

    successfully added deeleted funciton

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.badj8fj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +38,12 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/add-roommate/:id', async(req, res)=>{
+        const id  = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await roommateCollection.deleteOne(query);
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
