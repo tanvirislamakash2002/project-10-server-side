3814012fd1272a66a9974f8cd8d9bc8706ecf869
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-02-07 23:23:35 +0600

    added date while application

--- src/modules/application/application.controller.js
+++ src/modules/application/application.controller.js
@@ -1,13 +1,27 @@
 import { dbService } from "../../services/database.service.js"
 
 
-const application = async(req, res) => {
-    const applicationCollection = dbService.applications
-    const result = await applicationCollection.insertOne(req.body)
-    res.status(200).json({
-        success:true,
-        result
-    })
+const application = async (req, res) => {
+    try {
+        const applicationCollection = dbService.applications
+
+        const data = {
+            ...req.body,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+        }
+        const result = await applicationCollection.insertOne(data)
+        res.status(200).json({
+            success: true,
+            result
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
 }
 
 export const applicationController = {
